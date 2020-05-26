a7d33f8e2529087e42bebdce7ca47043df8ff21a
Andrew Palmer (CodeLifter) <a@codelifter.net>
2020-05-26 15:20:25 -0700

    updating database

--- CovidApi.CodeLifter.IO/Controllers/AdminController.cs
+++ CovidApi.CodeLifter.IO/Controllers/AdminController.cs
@@ -15,7 +15,7 @@ namespace CovidApi.CodeLifter.IO.Controllers
 {
     [PasswordResourceFilter]
     [ApiExplorerSettings(IgnoreApi = true)]
-        public class AdminController
+    public class AdminController
     {
         public const string GithubFolderPath = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/";
 

fa1529aceefc1a4f47a6ef000c8bf4f9b38c0771
Andrew Palmer <a@codelifter.net>
2020-06-05 15:25:20 -0700

    switching to latest

--- Scripts/docker_push.sh
+++ Scripts/docker_push.sh
@@ -5,14 +5,17 @@ echo "Creating migration tagged ${fileData}"
 echo 'PUSHING covidapi-codelifter-data'
 container_name=covidapi-codelifter-data
 docker commit -m "A working SQL Server container storing all the John's Hopkins Covid Data as of ${fileData}" -a "Andrew Palmer (CodeLifterIO)" ${container_name} codelifterio/${container_name}:${fileData} 
+docker push codelifterio/${container_name}:latest
 docker push codelifterio/${container_name}:${fileData} 
 
 echo 'PUSHING covidapi-codelifter-io'
 container_name=covidapi-codelifter-io
 docker commit -m "A working RESTful API to query all of Johns Hopkins Covid19 data as of ${fileData}.  Hard Dependency on codelifterio/covidapi-codelifter-data" -a "Andrew Palmer (CodeLifterIO)" ${container_name} codelifterio/${container_name}:${fileData} 
+docker push codelifterio/${container_name}:latest 
 docker push codelifterio/${container_name}:${fileData} 
 
 echo 'PUSHING covidapi-codelifter-admin'
 container_name=covidapi-codelifter-admin
 docker commit -m "A working admin tool to download all of Johns Hopkins Covid19 data as of ${fileData}.  Hard Dependency on codelifterio/covidapi-codelifter-data" -a "Andrew Palmer (CodeLifterIO)" ${container_name} codelifterio/${container_name}:${fileData} 
+docker push codelifterio/${container_name}:latest
 docker push codelifterio/${container_name}:${fileData} 
